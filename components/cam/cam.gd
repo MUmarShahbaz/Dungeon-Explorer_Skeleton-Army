a7dc4ba0f5531ed273857601aa310c6e385938ca
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-10-31 21:22:38 +0500

    Created CAM and Base Entity

---
+++ components/cam/cam.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+class_name CAM
+
+var target: CharacterBody2D
+@export var smoothing: float = 0.5
+@export var offset_distance: Vector2 = Vector2(120, 60)
+var target_offset: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	zoom = Vector2.ONE * 3.5
+	limit_bottom = 250
+	limit_smoothed = true
+	position_smoothing_enabled = true
+
+func _process(_delta: float) -> void:
+	var cam_off: Vector2 = Vector2.ZERO
+	cam_off.x = Input.get_axis("cam_left", "cam_right")
+	cam_off.y = Input.get_axis("cam_up", "cam_down")
+	set_target_offset(cam_off)
+	if target:
+		global_position = lerp(global_position, target.global_position + target_offset, smoothing)
+
+func set_target_offset(new_offset: Vector2):
+	target_offset.x = new_offset.x * offset_distance.x
+	target_offset.y = new_offset.y * offset_distance.y
---
+++ components/entity/entity.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+class_name Entity
+
+@export_group("Health", "HP")
+@export var HP_Health_Points : float = 100
+@export var HP_Regeneration_Rate : float = 1
+
+@export_group("Stamina", "SP")
+@export var SP_Stamina_Point : float = 100
+@export var SP_Regeneration_Rate : float = 1
+
+@export_group("Movement", "MV")
+@export var MV_Speed : float = 200
+@export var MV_Run_Speed : float = 300
+@export var MV_Jump : float = -300
+
+@export_group("Attack", "ATK")
+@export var ATK_Damage : float = 10
+@export var ATK_Primary_Combo_Length : int = 3
+
+var facing : int = 1
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
